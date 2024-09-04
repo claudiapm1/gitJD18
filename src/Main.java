193562881f65d2c0b333e0d70e252ddd774daf36
Claudia Patricia Martínez Guerrero <claudia.martinez@integral.interno>
2024-09-03 20:26:57 -0600

    Modifiqué el mensaje en el main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        String miTexto ="Bienvenidos a Java Developer";
+            //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        System.out.printf("Hello and welcome!" + "\n");
+        System.out.println("Hola,  = " + miTexto);
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
